66cc23ac35026913279e2b09c8547deb35bd9459
Egemen Turoglu <62505792+EgmTrg@users.noreply.github.com>
2022-10-17 23:41:58 +0300

    Question generating now

--- BTKAkademi/Assets/Scripts/GameManager.cs
+++ BTKAkademi/Assets/Scripts/GameManager.cs
@@ -10,12 +10,18 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject kare_Prefab;
     [SerializeField] private Transform kareler_Panel;
     [SerializeField] private Transform soru_Panel;
+    [SerializeField] private Text soru_Text;
+
     private GameObject[] kareler = new GameObject[25];
+    private List<int> degerler = new List<int>();
+    private int bolenSayi, bolunenSayi, kacinciSoru = 0;
+
 
     private void Start() {
         soru_Panel.GetComponent<RectTransform>().localScale = Vector3.zero;
         KareleriOlustur();
     }
+
 
     private void KareleriOlustur() {
         for (int i = 0; i < 25; i++) {
@@ -36,9 +42,21 @@ public class GameManager : MonoBehaviour
 
     private void DegerleriKarelereYaz() {
         foreach (var kare in kareler) {
-            kare.transform.GetChild(0).GetComponent<Text>().text = Random.Range(1, 12).ToString();
+            var deger = Random.Range(1, 12);
+            degerler.Add(deger);
+            kare.transform.GetChild(0).GetComponent<Text>().text = deger.ToString();
         }
     }
 
-    private void SoruPaneliniAc() => soru_Panel.GetComponent<RectTransform>().DOScale(1, 0.5f).SetEase(Ease.OutBack);
+    private void SoruPaneliniAc() {
+        SoruSor();
+        soru_Panel.GetComponent<RectTransform>().DOScale(1, 0.5f).SetEase(Ease.OutBack);
+    }
+
+    private void SoruSor() {
+        bolenSayi = Random.Range(2, 11);
+        kacinciSoru = Random.Range(0, degerler.Count);
+        bolunenSayi = bolenSayi * degerler[kacinciSoru];
+        soru_Text.text = bolunenSayi.ToString() + " : " + bolenSayi.ToString();
+    }
 }
